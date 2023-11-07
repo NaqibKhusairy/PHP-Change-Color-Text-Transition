82a603cf7a1da5856e6ca506cf910782114bb427
PUTERA NAQIB KHUSAIRI BIN ASRI <130439246+NaqibKhusairy@users.noreply.github.com>
2023-11-07 18:51:16 +0800

    Add files via upload

--- result.php
+++ result.php
@@ -1,36 +1,44 @@
+<?php
+if (isset($_POST['text_input'])) {
+    $text = $_POST['text_input'];
+    $totaltime = strlen($text) * 0.25;
+?>
 <!DOCTYPE html>
 <html>
 <head>
     <title>Text Transition Result</title>
     <link rel="stylesheet" type="text/css" href="style.css">
+    <style>
+        :root {
+            --totaltime: <?php echo $totaltime . "s"; ?>;
+        }
+    </style>
 </head>
 <body>
     <div class="text-container">
         <?php
-        if (isset($_POST['text_input'])) {
-            $text = $_POST['text_input'];
+        $colors = ["#ffffff", "#54BCDA", "#DA4167", "#C589E8"];
+        $delay = 0;
 
-            $colors = ["#ffffff", "#54BCDA", "#DA4167", "#C589E8"];
-            $delay = 0;
-
-            for ($i = 0; $i < strlen($text); $i++) {
-                if(strlen($text)<=5) {
-                    $time= 1/strlen($text);
-                }
-                else if(strlen($text)<=10) {
-                    $time= 2/strlen($text);
-                }
-                else{
-                    $time= 3/strlen($text);
-                }
-                $color = $colors[$i % count($colors)];
-                echo "<span style=\"--color: $color; --delay: ${delay}s\">{$text[$i]}</span>";
-                $delay += $time;
-            }
-        } else {
-            echo "Please enter text in the input form.";
+        for ($i = 0; $i < strlen($text); $i++) {
+            $color = $colors[$i % count($colors)];
+            echo "<span style=\"--color: $color; --delay: ${delay}s\">{$text[$i]}</span>";
+            $delay += 0.25;
         }
         ?>
     </div>
+    <?php
+    } else {
+        echo "
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Text Transition Result</title>
+    <link rel='stylesheet' type='text/css' href='style.css'>
+</head>
+<body>
+    <h1>Please enter text in the input form.</h1>";
+    }
+    ?>
 </body>
 </html>
